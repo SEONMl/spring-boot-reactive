7e4e749163ca326238fac75700c21e4d67a9f25e
SEONMl <sm000117@naver.com>
2023-03-07 15:15:33 +0900

    간단한 Reactive 웹 애플리케이션 만들기

---
+++ src/main/java/com/example/springbootreactive/KitchenService.java
@@ -0,0 +1,28 @@
+package com.example.springbootreactive;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class KitchenService {
+    private List<Dish> menu = Arrays.asList(new Dish("Sesame chicken"),
+            new Dish("Lo mein noodles"),
+            new Dish("Sweet & sour beef"));
+
+    private Random picker = new Random();
+
+    Flux<Dish> getDishes(){
+        return Flux.<Dish> generate(sink -> sink.next(randomDish()))
+                .delayElements(Duration.ofMillis(250));
+    }
+
+    private Dish randomDish() {
+        return menu.get(picker.nextInt(menu.size()));
+    }
+
+}
