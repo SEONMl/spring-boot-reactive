3a4af7116882a248095dbba91734c5cea6031101
SEONMl <sm000117@naver.com>
2023-03-07 19:45:25 +0900

    직접 쿼리문 작성

--- src/main/java/com/example/springbootreactive/repository/ItemRepository.java
+++ src/main/java/com/example/springbootreactive/repository/ItemRepository.java
@@ -1,7 +1,9 @@
 package com.example.springbootreactive.repository;
 
 import com.example.springbootreactive.domain.Item;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
     /**
@@ -10,4 +12,12 @@ public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
      * existsById(), count()
      * deleteById(), delete(), deleteAll()
      */
+
+    Flux<Item> findByNameContaining(String partialName);
+
+    @Query("{'name':  ?0, 'age': ?1}")
+    Flux<Item> findItemForCustomerMonthlyReport(String name, int age);
+
+    @Query(sort="{'age': -1}")
+    Flux<Item> findSortedStuffForWeeklyReport();
 }
