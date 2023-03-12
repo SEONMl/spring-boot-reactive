1289662a7e3c5ac2d09d858f42909db4b704bf0a
SEONMl <sm000117@naver.com>
2023-03-12 20:24:09 +0900

    R소켓 request-stream 방식의 클라이언트

--- src/main/java/com/example/springbootreactive/controller/RSocketController.java
+++ src/main/java/com/example/springbootreactive/controller/RSocketController.java
@@ -1,14 +1,18 @@
 package com.example.springbootreactive.controller;
 
 import com.example.springbootreactive.domain.Item;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.time.Duration;
 
 import static io.rsocket.metadata.WellKnownMimeType.MESSAGE_RSOCKET_ROUTING;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON;
@@ -35,5 +39,13 @@ public class RSocketController {
                         .data(item)
                         .retrieveMono(Item.class))
                 .map(saved -> ResponseEntity.created(URI.create("/items/request-response")).body(saved));
+    }
+
+    @GetMapping(value = "/request-stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<Item> findItemsUsingRSocketRequestStream() {
+        return requester.flatMapMany(requester -> requester
+                .route("newItems.request-stream")
+                .retrieveFlux(Item.class)
+                .delayElements(Duration.ofSeconds(1)));
     }
 }
